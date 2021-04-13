d32abbbc84f70661d556dabd4376aed00442c48d
ingsuifon <ingsuifon@vip.qq.com>
2021-04-13 22:01:03 +0800

    first

---
+++ src/main/java/com/example/neudemo/NeudemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.neudemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeudemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeudemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/neudemo/NeudemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neudemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeudemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
